dafbbb777eb993e8e0814787275ed59323d2c91f
Julius B <36518501+silver1bear@users.noreply.github.com>
2022-01-28 11:49:50 +0100

    added firebase project, trying out https-fetch

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,32 @@
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
 	event.waitUntil(
-	  caches.open(cacheName).then(function(cache) {
-		return cache.addAll(
-		  [
-			'/index.html',
-			'/apple-touch-icon-192.png',
-			'/apple-touch-icon.png'
-		  ]
-		);
-	  })
+		caches.open('sw').then(function (cache) {
+			return cache.addAll(
+				[
+					'/index.html',
+					'/apple-touch-icon-192.png',
+					'/apple-touch-icon.png',
+					'/favicon-32.ico',
+					'/manifest.json'
+				]
+			);
+		})
 	);
-  });
+});
+
+self.addEventListener('fetch', (e) => {
+	e.respondWith((async () => {
+		const r = await caches.match(e.request);
+		console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+
+		if (r) { return r; }
+		
+		const response = await fetch(e.request);
+		const cache = await caches.open("sw");
+		
+		console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+		cache.put(e.request, response.clone());
+		
+		return response;
+	})());
+});
